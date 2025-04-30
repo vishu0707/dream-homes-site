05fb6c8fefc4f1fc7bc1309de0b1bea313b7028b
vishu0707 <deeptanugade5881@gmail.com>
2025-04-30 23:53:29 +0530

    fix react import error

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { assets } from "../assets/assets";
 import { motion } from "framer-motion";
 
@@ -31,7 +30,7 @@ const About = () => {
           <div className="grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28">
             <div>
               <p className="text-4xl font-medium text-gray-800">10+</p>
-              <p>Years of Excellence.</p>
+              <p>Years of Excellence</p>
             </div>
             <div>
               <p className="text-4xl font-medium text-gray-800">12+</p>
